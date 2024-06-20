a7a01511a0dbb74ba65df9e54291203018a378a5
henryy-el <phonghngle@gmail.com>
2024-06-21 02:04:36 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-xl font-medium ">react starter pack</h1>
+    </div>
+  )
+}
+
+export default App
